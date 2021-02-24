4d214470bf7083937a321915f4ff5b19a12aca88
xenx96 <75344540+xenx96@users.noreply.github.com>
2021-02-24 11:25:39 +0900

    add data

--- R/Cluster.r
+++ R/Cluster.r
@@ -36,7 +36,7 @@ data$차명[data$차명==""] <- Null_car_name
 
 
 #Numeric Values change with Car_name ; Not Company Name. Using by Aggregate
-df<-aggregate(전장~차명,data,mean)
+df <- aggregate(전장~차명,data,mean)
 df <- merge(df,aggregate(전고~차명,data,mean),by='차명')
 df <- merge(df,aggregate(전폭~차명,data,mean),by='차명')
 
@@ -52,18 +52,19 @@ df$vh <- df$전장*df$전폭
 plot(df)
 
 #Kmeans Clustering
-##소형/중형/대형/봉고/트럭
-df_kmeans = kmeans(df[,c("전고", "vh")], 5) 
+##소형/중형/대형/봉고/
+df_kmeans = kmeans(df[,c("전고", "vh")], 4) 
 
 #Counter Cluster
 table(df_kmeans$cluster)
 
+#add K-Means-Cluster to df$cluster
 df$cluster <- df_kmeans$cluster %>% as.character
+
 #ggplot
-
-
 (car_graph<-ggplot(data=df, aes(x=vh, y=전고, colour=cluster,group = 차명)) + 
   geom_point(shape=19, size=4) + 
   ggtitle("Scatter Plot of CarData' K-means clusters")) %>% ggplotly
 
+write.csv(df,'1.for_kmeans_df.csv')
 
